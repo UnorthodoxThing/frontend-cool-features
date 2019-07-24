9ad2a9a2912239226cab0dedc96b508c4b036de1
Caroline Rozali <UnorthodoxThing@Outlook.com>
2019-07-24 19:59:54 +1000

    Add scripts

--- scrolling-animations/scrolling-animations-01/script.js
+++ scrolling-animations/scrolling-animations-01/script.js
@@ -1,1 +1,7 @@
 let image = document.querySelector("#target-image");
+
+window.addEventListener('scroll', function() {
+  // Adjust speed here -> Edit the integer 3. Higher for slower, and lower for faster.
+  let scroll = window.pageYOffset / 3;
+  image.style.transform = `rotate(${scroll}deg)`;
+});
