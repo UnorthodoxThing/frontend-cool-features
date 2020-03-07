b0017e4860e2bcb10111ea3f6e39ce26acb6cd4a
Caroline Rozali <UnorthodoxThing@Outlook.com>
2020-03-07 16:00:27 +0700

    Initial commit

---
+++ image-gallery/image-gallery-02/README.md
@@ -0,0 +1,9 @@
+# Image Gallery 02: Using Event Delegation
+
+More improved on performance than 'image-gallery-01', because it does not create 1000s of add event listeners,
+
+## Brief
+
+## Credits
+
+- _MDN_ - Image Gallery
---
+++ image-gallery/image-gallery-02/script.js
@@ -0,0 +1,14 @@
+const displayedImage = document.querySelector('.display-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+// Looping through images
+// how many images
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', images src );
+thumbBar.appendChild(newImage);
+
+// Wiring up the Darken/ Lighten button
